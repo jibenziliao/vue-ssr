df285109cb2885b813b66b9dd6a126ed615b9ed4
jibenziliao <1501718947@qq.com>
2018-10-26 11:23:23 +0800

    feat:update webpack config

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -29,7 +29,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            // presets: ['stage-0'],
+            presets: ['stage-0'],
             plugins: ['syntax-dynamic-import']
           }
         }
