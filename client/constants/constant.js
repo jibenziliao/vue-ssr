650b3df72c50c149657d64b192d8d8538e470564
jibenziliao <1501718947@qq.com>
2018-10-30 21:10:06 +0800

    feat:update request url

--- client/constants/constant.js
+++ client/constants/constant.js
@@ -1,5 +1,5 @@
 // 真实接口地址为https://api.yunlu6.com 这里线上使用了nginx反向代理。
-export const API_URL = process.env.NODE_ENV === 'development' ? '' : 'https://vue-ssr.keyy.xyz/nginx_proxy'
+export const API_URL = process.env.NODE_ENV === 'development' ? '' : 'https://vue-ssr.keyy.xyz'
 export const REQUEST_TIME_OUT = 30000
 export const COUNT_DOWN_SECOND = 60
 export const AUTHORIZATION_TIME = 120
